99182fc15c6599d4361e9089d9af8946cae21a8c
Valeriia Siabruk <vsyabryk@gmail.com>
2022-07-20 13:51:47 +0300

    add search country functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+import fetchCountries from './js/fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+const listBlockEl = document.querySelector('.country-list');
+const infoBlockEl = document.querySelector('.country-info');
+
+const inputHandler = debounce(({ target: { value } }) => {
+  value = value.trim();
+
+  if (!value) return;
+
+  fetchCountries(value)
+    .then(data => {
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+
+      renderMarkup(data);
+    })
+    .catch(() => {
+      Notify.failure('Oops, there is no country with that name');
+      clearMarkup();
+    });
+}, DEBOUNCE_DELAY);
+
+inputEl.addEventListener('input', inputHandler);
+
+const clearMarkup = () => {
+  infoBlockEl.innerHTML = '';
+  listBlockEl.innerHTML = '';
+};
+
+const renderMarkup = data => {
+  clearMarkup();
+
+  if (data.length === 1) {
+    infoBlockEl.insertAdjacentHTML('afterbegin', createInfoMarkup(data));
+  } else {
+    listBlockEl.insertAdjacentHTML('afterbegin', createListMarkup(data));
+  }
+};
+
+const createListMarkup = data => {
+  return data
+    .map(
+      ({ name: { official }, flags: { svg } }) =>
+        `<li><img src="${svg}" alt="${official}" width="30" height="20">${official}</li>`
+    )
+    .join('');
+};
+
+const createInfoMarkup = data => {
+  return data.map(
+    ({ name: { official }, capital, population, flags: { svg }, languages }) =>
+      `<h1><img src="${svg}" alt="${official}" width="30" height="20">${official}</h1>
+      <p><strong>Capital:</strong> ${capital}</p>
+      <p><strong>Population:</strong> ${population}</p>
+      <p><strong>Languages:</strong> ${Object.values(languages)}</p>`
+  );
+};
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,16 @@
+function fetchCountries(search) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${search}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data;
+    });
+}
+
+export default fetchCountries;
